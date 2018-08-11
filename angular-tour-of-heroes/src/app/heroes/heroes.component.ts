ae42c1c02210894ed7acd5c05b5790f7fe31930f
alesandro.knoll <alesandro.knoll@transporeon.com>
2018-08-11 21:57:30 +0200

    changed mock array to Observable with subscribe function

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from "../hero";
-import {HEROES} from "../mock-heroes";
+import { Hero } from '../hero';
+import {HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,18 +11,22 @@ export class HeroesComponent implements OnInit {
 
 
   selectedHero: Hero;
+  heroes: Hero[];
 
-  onSelect(hero: Hero): void{
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
-
-
-  constructor() { }
-
-  ngOnInit() {
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
