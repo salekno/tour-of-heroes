9cfa487a371a27885e1f073763df4cb301125620
alesandro.knoll <alesandro.knoll@transporeon.com>
2018-08-11 22:40:59 +0200

    removed dead code

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import {HeroService } from './hero.service';
 export class HeroesComponent implements OnInit {
 
 
-  selectedHero: Hero;
   heroes: Hero[];
 
 
@@ -20,9 +19,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
