77dd32e3d0cf19755b7b301d3fdf62698c0d93eb
alesandro.knoll <alesandro.knoll@transporeon.com>
2018-08-11 22:05:39 +0200

    injected messageService to messages component

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -12,7 +12,7 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    //TODO:  send the message _after_ fetching the heroes
+    // TODO:  send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
